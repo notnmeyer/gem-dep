e1276b4681052edd1107e61d0821c658c6977db2
Nate Meyer <nmeyer@gmail.com>
2015-10-06 17:15:28 -0700

    model reality a little better

--- libraries/test.rb
+++ libraries/test.rb
@@ -1,7 +1,11 @@
 module SomeModule
+  require "chef/resource/chef_gem"
+  Chef::Resource::ChefGem.new("fog").run_action(:install)
+  require "fog"
+
   class SomeClass
-    require "chef/resource/chef_gem"
-    Chef::Resource::ChefGem.new("fog").run_action(:install)
-    require "fog"
+    def initialize
+      true
+    end
   end
 end
